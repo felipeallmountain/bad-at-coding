a882e9492a7b003a13381c4cbc69cabf126cf95d
felipeallmountain <felipeallmountain@gmail.com>
2026-07-06 22:24:23 -0500

    add paragraph animations

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,8 @@ import ScrambleTextPlugin from 'gsap/ScrambleTextPlugin'
 
 const CONTENT_CONTAINER = '.container'
 const HEADER_CONTAINER = '.header'
-const CONTAINER_TITLE = '.container__title h1'
+const CONTAINER_HEAD = '.container__title'
+const CONTAINER_TITLE = `${CONTAINER_HEAD} h1`
 const CONTAINER_PARAGRAPH = '.container__paragraph'
 
 gsap.registerPlugin(Flip)
@@ -82,57 +83,43 @@ class App {
     }
 
     try {
-      // this.contentContainer.classList.add('is-loading')
-      // this.contentContainer.replaceChildren()
-
-      // const loader = document.createElement('div')
-      // loader.className = 'container__loader'
-      // loader.innerHTML = `
-      //   <div class="container__loader-terminal">
-      //     <div class="container__loader-text">> RETRIEVING DATA...</div>
-      //     <div class="container__loader-bar">
-      //       <div class="container__loader-progress"></div>
-      //     </div>
-      //   </div>
-      // `
-      // this.contentContainer.appendChild(loader)
       const oldTitle = this.contentContainer.querySelector(CONTAINER_TITLE)
       const tl = gsap.timeline()
 
-
       if (oldTitle) {
-        tl.fromTo(oldTitle, {
-          scrambleText: oldTitle.innerHTML
-        }, {
+        tl.to(oldTitle, {
           duration: 5,
+          ease: 'power2.out',
           repeat: -1,
           scrambleText: {
-            text: '',
-            // speed: 2,
-            chars: 'upperAndLowerCase',
+            text: oldTitle.innerHTML,
+            chars: oldTitle.innerHTML,
+            speed: .5,
             rightToLeft: true,
-            revealDelay: 1,
-          }
+          },
         })
       }
 
+      const oldParagraph = this.contentContainer.querySelector(CONTAINER_PARAGRAPH)
+      const oldParagraphTl = gsap.timeline()
+      if (oldParagraph) {
+        const oldFragments = oldParagraph.querySelectorAll('p')
+        const oldFragmentsList = Array.from(oldFragments)
 
-
-
-
-      // tl.to(oldTitle, {
-      //   // scrambleText: oldTitle?.innerHTML
-      //   duration: 5,
-      //   scrambleText: {
-      //     // text: oldTitle?.innerHTML || '',
-      //     text: '_',
-      //     speed: 2,
-      //     chars: 'upperAndLowerCase',
-      //     rightToLeft: true,
-      //     revealDelay: 1,
-      //   },
-      //   repeat: -1,
-      // })
+        oldFragmentsList.reverse().forEach(frag => {
+          gsap.to(frag, {
+            duration: 5,
+            ease: 'power2.out',
+            repeat: -1,
+            scrambleText: {
+              chars: 'upperAndLowerCase',
+              text: frag.textContent,
+              speed: 1,
+              rightToLeft: true,
+            }
+          })
+        })
+      }
 
       const response = await fetch(url, {
         headers: {
@@ -142,7 +129,6 @@ class App {
       if (!response.ok) {
         throw new Error(`Failed to load page: ${response.status}`)
       }
-
 
       const htmlText = await response.text()
       const parser = new DOMParser()
@@ -155,17 +141,55 @@ class App {
       if (newTitle) {
         tl.kill()
         gsap.to(oldTitle, {
-          duration: .5,
+          duration: .6,
+          ease: 'power2.in',
           scrambleText: {
             text: newTitle.innerHTML,
-            speed: 2
+            speed: 4,
           }
         })
       }
-      // this.contentContainer.replaceChildren()
 
       if (newContent) {
         const state = Flip.getState(this.contentContainer)
+        const newParagraph = newContent.querySelector(CONTAINER_PARAGRAPH)
+        if (newParagraph) {
+          const paragraphFragments = newParagraph.querySelectorAll('p')
+          const fragmentList = Array.from(paragraphFragments)
+          const containerHead = this.contentContainer.querySelector(CONTAINER_HEAD)
+          if (fragmentList.length > 0 && containerHead) {
+
+            let newParagraph
+            if (oldParagraph) {
+              oldParagraph.replaceChildren()
+              newParagraph = oldParagraph
+            } else {
+              const emptyParagraph = document.createElement('div')
+              emptyParagraph.className = 'container__paragraph code-command-lg text-color-default'
+              containerHead.append(emptyParagraph)
+              newParagraph = emptyParagraph
+            }
+            oldParagraphTl.kill()
+            oldParagraphTl.delay(.6)
+
+            fragmentList.forEach(fragment => {
+              const paragraph = document.createElement('p')
+              newParagraph.append(paragraph)
+              oldParagraphTl.to(paragraph, {
+                duration: .4,
+                ease: 'power2.in',
+                scrambleText: {
+                  text: fragment.textContent,
+                  speed: 4
+                }
+              })
+            })
+          }
+        } else {
+          if (oldParagraph) {
+            oldParagraph.remove()
+          }
+        }
 
         // Update template dataset
         const oldTemplate =
@@ -220,6 +244,12 @@ class App {
       // Remove transitioning class for entrance animation
       // this.contentContainer.classList.remove('is-transitioning')
     }
+  }
+
+  private createEmptyParagraph() {
+    const emptyParagraph = document.createElement('div')
+    emptyParagraph.className = 'container__paragraph code-command-lg text-color-default'
+
   }
 }
 
